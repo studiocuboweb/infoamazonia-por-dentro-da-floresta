ee4bf60ab984768060623d483fe55d40091230e2
Caio <alcantaracaiolucas@gmail.com>
2018-04-09 06:31:48 +0100

    chore: Update RelatedNews style

--- src/components/RelatedNews.js
+++ src/components/RelatedNews.js
@@ -1,34 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from 'prop-types';
+import { media } from '../styles/utils';
 
 const Wrapper = styled.div`
-  margin: 0 1rem;
-    @media (min-width: 75em) {
-      margin: 0;
-      margin-left: 10vw;
-      margin-right: 10vw;
-      .more-content {
-        .related-content {
-          h3 {
-            font-size: 1rem;
-          }
-          p {
-            font-size: 1rem;
-          }
-        }
-      }
-    }
-    .more-content-header {
-      border-bottom: 1px rgba(0,0,0,.3) solid;
-      padding-bottom: 0.5rem;
-      margin-bottom: 1.3rem;
-    }
+  margin: 0 auto;
+  max-width: 500px;
+  padding: 1rem 1rem 3rem;
     .more-content {
       a {
         max-height: 12rem;
         display: flex;
-        justify-content: space-between;
       }
       .related-content {
         margin-left: .5rem;
@@ -37,15 +19,57 @@ const Wrapper = styled.div`
           font-size: .9rem;
         }
         p {
-          font-size: .7rem;
+          font-size: .8rem;
           color: #333;
         }
       }
       img {
         height: 4.8rem;
-        margin: 0;
+        margin: .5rem 0;
       }
     }
+  ${media.phablet`
+    .more-content {
+      a {
+        max-height: 12rem;
+        display: flex;
+      }
+      .related-content {
+        h3 {
+          font-size: 1.2rem;
+        }
+        p {
+          font-size: 1rem;
+        }
+      }
+      img {
+        height: 6rem;
+        margin: 0;
+        margin-top: .5rem;
+      }
+    }
+  `}
+  ${media.desktop`
+    margin-left: 8vw;
+    margin-right: 8vw;
+  `}
+  @media (min-width: 75em) {
+    margin: 0 auto;
+    max-width: 640px;
+    .more-content {
+      .related-content {
+        font-size: 1.2em;
+        h3 {
+          font-size: 1.4rem;
+        }
+      }
+    }
+  }
+  .more-content-header {
+    border-bottom: 1px rgba(0,0,0,.3) solid;
+    padding-bottom: 0.5rem;
+    margin-bottom: 1.3rem;
+  }
 `
 
 const RelatedNews = ({ sectionTitle, title, subtitle, imageUrl, href }) => {
