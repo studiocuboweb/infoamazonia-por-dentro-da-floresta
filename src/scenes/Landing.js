d056439d53d30484657376233069ff7af1f2751c
André Mota <andre.mota@studiocuboweb.com.br>
2018-09-12 17:08:58 -0300

    logo branco do infoamazonia na capa

--- src/scenes/Landing.js
+++ src/scenes/Landing.js
@@ -276,7 +276,7 @@ class Scene extends Component {
         <Top>
           <div className="partners">
             <img src={require("images/partners/amazonia-real.png")} />
-            <img src={require("images/partners/infoamazonia.png")} />
+            <img src={require("images/partners/infoamazonia-branco.png")} />
           </div>
           <SiteTitle />
           <h2>
