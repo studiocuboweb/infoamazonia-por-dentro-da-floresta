953fe407f9ef3b1ea07cd19fc65678fdcdf3f8fd
Paulo Eduardo Campos Afonso <pauloeduardocamposafonso@Akhenaton.local>
2019-04-23 09:01:32 -0300

    11.1 [ajuste] Trocar imagem da capa por outra imagem

--- src/scenes/Landing.js
+++ src/scenes/Landing.js
@@ -33,10 +33,11 @@ const Wrapper = styled.section`
     left: 0;
     bottom: 0;
     right: 0;
-    background-image: url(${require("images/bg-1.jpg")});
+    background-image: url(${require("images/bg.jpg")});
     background-size: cover;
     background-position: center;
     z-index: -1;
+    opacity:0.7;
   }
   &.route-transition-enter {
     opacity: 1;
